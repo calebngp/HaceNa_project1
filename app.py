3df5bd14ff27275f82c413dff88457bc0a86bd9c
Enrique Zarate <eszg.fury@gmail.com>
2022-05-03 16:36:43 -0400

    cambio en app.py

--- app.py
+++ app.py
@@ -1,53 +1,104 @@
-from flask import Flask, render_template, url_for #Importamos las librerias a ser usadas
-import requests
+# Flask para aplicacion web
+from flask import Flask, render_template, request, redirect, url_for
+# SQLAlchemy como ORM
+from flask_sqlalchemy import SQLAlchemy
 
-hacena = Flask(__name__)  #Creabamos el objetoi hacena
 
 
-@hacena.route("/")     #Llamabamos al metodo route y le pasamos el argumento slug o url
+app = Flask(__name__)
+########### Para crear la Base de Datos ###########
+# En consola : 
+# sqlite3 database/task.db
+# *Ya tener el directorio /database
+
+# Instancia la base de datos - /// -> Para que use el lenguak¿je de sqlite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/registrate.db'
+
+db = SQLAlchemy(app)
+
+# Modelo de la base de datos
+# class Registro(db.Model):
+#     id        = db.Column(db.Integer, primary_key=True)
+#     nombre     = db.Column(db.String(200))
+#     apellido   = db.Column(db.String(200))
+#     contrasena   = db.Column(db.String(200))
+#     email   = db.Column(db.String(200))
+#     done        = db.Column(db.Boolean)
+#     def __init__
+    
+class Contrata(db.Model):
+    id        = db.Column(db.Integer, primary_key=True)
+    descripcion_del_trabajo  = db.Column(db.String(200))
+    categoria_del_trabajo   = db.Column(db.String(200))
+    modalidad   = db.Column(db.String(200))
+    monto   = db.Column(db.Integer())
+    done        = db.Column(db.Boolean)
+
+class Ensenana(db.Model):
+    id        = db.Column(db.Integer, primary_key=True)
+    denominacion_del_curso   = db.Column(db.String(200))
+    descripcion_del_curso   = db.Column(db.String(200))
+    modulos   = db.Column(db.String(200))
+    duracion   = db.Column(db.Integer())
+    area   = db.Column(db.String(200))
+    done        = db.Column(db.Boolean)
+
+
+
+
+
+# Para renderizar la vista
+
+@app.route("/")     #Llamabamos al metodo route y le pasamos el argumento slug o url
 def inicio(): #Creamos la funcion inicio
+    #datos = Registro.query.all()
     return render_template('inicio.html')
-                #Retornamos la renderizacion de un documento
+            #Retornamos la renderizacion de un documento
 
-@hacena.route("/trabajana")
-def trabajana():
-    return render_template('trabajana.html')
-
-@hacena.route("/contratana")
+@app.route("/contrataNabuscar")
 def contratana():
-    return render_template('contratana.html')
+    datos1 = Contrata.query.all()
+    return render_template('contrataNa_Buscar_o_publicar.html')
 
-@hacena.route("/aprendena")
-def aprendena():
-    return render_template('aprendena.html')
-
-@hacena.route("/ensenana")
+@app.route("/ensenana", methods=['POST'])
 def ensenana():
+    datos1 = Ensenana.query.all()
+    datos1 = Ensenana(denominacion_del_curso = request.form['denominacion del curso'], descripcion_del_curso = request.form['descripcion del curso'], modulos = request.form['modulos'], duracion = request.form['duracion'], area = request.form['area'], done=False)
+    db.session.add(datos1)
+    db.session.commit()
     return render_template('ensenana.html')
 
-@hacena.route("/ingresana")
+@app.route("/ingresana", methods=["POST"])
 def ingresana():
-    return render_template('ingresana.html')
+    datos2 = Registro.query.all()
 
-@hacena.route("/registratena")
+    datos2 = Registro(email = request.form['email'], contrasena = request.form['contrasena'], done=False)
+    db.session.add(datos2)
+    db.session.commit()
+    return render_template('ingresana2.html')
+
+@app.route("/registratena", methods=['POST'])
 def registratena():
-    return render_template('registratena.html')
+    datos3 = Registro.query.all()
 
-@hacena.route("/informatena")
-def informatena():
-    return render_template('informatena.html')
+    datos3 = Registro(nombre = request.form['nombre'], apellido = request.form['apellido'], contrasena = request.form['contrasena'], email = request.form['email'], done=False)
+    db.session.add(datos3)
+    db.session.commit() # Para que lo guarde/ejecute
+    return redirect(url_for('registratena.html'))
 
-@hacena.route("/contratana_menu")
-def contratana_menu():
-    return render_template('contratana_menu.html')
+@app.route("/formulario", methods=['POST'])
+def formulario():
+    datos4 = Registro(nombre = request.form['nombre'], apellido = request.form['apellido'], contrasena = request.form['contrasena'], email = request.form['email'], done=False)
+    db.session.add(datos4)
+    db.session.commit() # Para que lo guarde/ejecute
+    return redirect(url_for('formulario_general_registro.html'))
+@app.route("/contrataNaregistro", methods=['POST'])
+def contratareg():
+    datos5 = Contrata(descripcion_del_trabajo = request.form['descripcion_trabajo'], categoria_del_trabajo = request.form['categoria_trabajo'], modalidad = request.form['modalidad'], monto = request.form['monto'], done=False)
+    db.session.add(datos5)
+    db.session.commit() # Para que lo guarde/ejecute
+    return redirect(url_for('registratena.html'))
 
-@hacena.route("/contactana")
-def contactana():
-    return render_template('contactana.html')
-
-@hacena.route("/profile_card")
-def profile_card():
-    return render_template('profile_card.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
